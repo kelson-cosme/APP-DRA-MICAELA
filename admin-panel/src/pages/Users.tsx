e7091d2022a18ca05cbf6f0d0be466c7273e84ae
kelson-cosme <kelson.almeida123@gmail.com>
2026-03-07 00:38:58 -0400

    feat: debug

--- admin-panel/src/pages/Users.tsx
+++ admin-panel/src/pages/Users.tsx
@@ -8,7 +8,6 @@ import {
     DialogDescription,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
     DialogFooter,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
