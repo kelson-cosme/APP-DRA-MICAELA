fda9aad647b4f1d6d5cd25a561ef10b4ee0c1fcc
kelson-cosme <kelson.almeida123@gmail.com>
2026-03-10 13:51:25 -0400

    returando alguns arquivos

--- admin-panel/add_comment_parent_id.sql
+++
@@ -1,3 +0,0 @@
--- Add parent_id column to comments table to support nested replies
-alter table public.comments 
-add column if not exists parent_id uuid references public.comments(id) on delete cascade;
--- admin-panel/add_image_position.sql
+++
@@ -1,2 +0,0 @@
--- Adicionando a coluna image_position na tabela events
-ALTER TABLE events ADD COLUMN IF NOT EXISTS image_position VARCHAR(50) DEFAULT 'center';
